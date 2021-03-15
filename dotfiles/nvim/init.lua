6ff27e64040e5b7e620857097d1c3431f23129fc
Marco Valtas <mavcunha@gmail.com>
2021-03-14 20:52:40 -0400

    Init lua file for NeoVIM

---
+++ dotfiles/nvim/init.lua
@@ -0,0 +1,37 @@
+
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+
+cmd 'packadd paq-nvim'               -- load the package manager
+local paq = require('paq-nvim').paq  -- a convenient alias
+paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
+paq 'sheerun/vim-polyglot'           -- syntax highlighting
+
+-- work around while https://github.com/neovim/neovim/pull/13479 is open
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+local function opt(scope, key, value)
+  scopes[scope][key] = value
+  if scope ~= 'o' then scopes['o'][key] = value end
+end
+
+cmd 'colorscheme torte'
+
+local indent = 2
+opt('b', 'expandtab', true)                           -- Use spaces instead of tabs
+opt('b', 'shiftwidth', indent)                        -- Size of an indent
+opt('b', 'smartindent', true)                         -- Insert indents automatically
+opt('b', 'tabstop', indent)                           -- Number of spaces tabs count for
+opt('o', 'hidden', true)                              -- Enable modified buffers in background
+opt('o', 'joinspaces', false)                         -- No double spaces with join after a dot
+opt('o', 'scrolloff', 4 )                             -- Lines of context
+opt('o', 'shiftround', true)                          -- Round indent
+opt('o', 'sidescrolloff', 8 )                         -- Columns of context
+opt('o', 'smartcase', true)                           -- Don't ignore case with capitals
+opt('o', 'splitbelow', true)                          -- Put new windows below current
+opt('o', 'splitright', true)                          -- Put new windows right of current
+opt('o', 'termguicolors', true)                       -- True color support
+opt('o', 'wildmode', 'list:longest')                  -- Command-line completion mode
+opt('w', 'number', true)                              -- Print line number
+opt('w', 'relativenumber', true)                      -- Relative line numbers
+opt('w', 'wrap', false)
