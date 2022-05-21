74f981dbfeaacfa8061610f877b94b7ac172b57c
Marco Valtas <mavcunha@gmail.com>
2022-05-21 11:56:18 -0400

    Remove deprecated option from nvim-treesitter

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -80,7 +80,6 @@ require('telescope').setup{
 
 -- treesitter configuration
 require('nvim-treesitter.configs').setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers)
   highlight = {
     enable = true,
   },
